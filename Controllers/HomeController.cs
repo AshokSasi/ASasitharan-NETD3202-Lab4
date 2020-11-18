61bbc522323bb99fa6e9a2e6642ab6bdafae77da
ASHOK SASITHARAN <ASHOK2801@hotmail.com>
2020-11-17 21:15:00 -0500

    Added appraised price functionality and failure view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,14 +9,31 @@ namespace ASasitharan_NETD3202_Lab4.Controllers
 {
     public class HomeController : Controller
     {
+        //create new list of book
+        List<book> bookList = new List<book>();
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult Appraise()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Appraise(book textbook)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewData["Message"] = textbook.ToString();
+
+                
+                    return View("ViewAppraisal", textbook);
+            }
+            else
+            {
+                return View("Failure");
+            }
         }
     }
 }
---
+++ Models/book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASasitharan_NETD3202_Lab4.Models
+{
+    public class book
+    {
+        //data members
+        public string title { get; set; }
+
+        public Int64 isbn { get; set; }
+
+        public double version { get; set; }
+        public double purchasePrice { get; set; }
+
+        public string condition { get; set; }
+
+        //default constructor
+        public book()
+        {
+
+        }
+
+        //parameterized constructor
+        public book(string title, int isbn, double version, double purchasePrice, string condition)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.version = version;
+            this.purchasePrice = purchasePrice;
+            this.condition = condition;
+        }
+
+        //method that calculates the value of the textbook based on the condition
+        public double calculateValue (double origPrice ,string condition)
+        {
+            if(condition.Equals("likeNew"))
+            {
+                return (origPrice / 2);
+            }
+            else if(condition.Equals("good"))
+            {
+                return (origPrice / 3);
+            }
+            else
+            {
+                return (origPrice / 4);
+            }
+            
+        }
+
+        //overrided ToString that displays the price of the book and details
+        public override string ToString()
+        {
+            return "Your textbook: " + this.title + ", Version: " + this.version + " was appraised at: $" + calculateValue(this.purchasePrice,this.condition);
+        }
+    }
+}
